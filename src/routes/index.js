70626be439a8deb6499b37d642e3d984172d58b0
Tomás Freudenberg <tommyefr@gmail.com>
2019-05-13 15:36:28 -0400

    feat: add Groups component

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { Switch, Route} from 'react-router-dom';
-import Loadable from 'react-loadable';
-import NotFound from './notFound';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Loadable from "react-loadable";
+import NotFound from "./notFound";
 
 const Home = Loadable({
-  loader: () => import('../components/home'),
+  loader: () => import("../components/home"),
+  loading: () => <div>Loading...</div>
+});
+
+const Groups = Loadable({
+  loader: () => import("../components/groups"),
   loading: () => <div>Loading...</div>
 });
 
 const Routes = () => (
   <div>
     <Switch>
-      <Route exact path="/" component={Home}/>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/groups" component={Groups} />
       <Route component={NotFound} />
     </Switch>
-      
   </div>
 );
 
