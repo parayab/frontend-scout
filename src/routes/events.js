d9329485482ebe58e3fa531d5097876f248d31bc
Tomás Freudenberg <tommyefr@gmail.com>
2019-05-14 22:25:07 -0400

    feat: add group event sidebar

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,18 +1,33 @@
-import React, { Component, Fragment } from 'react';
-import Layout from '../components/layout/layout';
+import React, { Component, Fragment } from "react";
+import { SidebarPushable, Sidebar, Menu, Icon } from "semantic-ui-react";
 
 class EventsView extends Component {
   constructor(props) {
-	super(props);
-	this.state = {
-    events: [],
-    loading: false
-	}
+    super(props);
+    this.state = {
+      groupEvents: [],
+      loadingGroupEvents: true,
+      groupId: 1
+    };
+    this.getAllGroupEvents = this.getAllGroupEvents.bind(this);
   }
-  
-	componentDidMount() {
+
+  async getAllGroupEvents() {
+    const response = await fetch("/groups/1/groupevent");
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      this.setState({
+        groupEvents: jsonResponse.groupEvents,
+        loadingGroupEvents: false
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.getAllGroupEvents();
+
     // this.setState({ loading: true });
-	// 	fetch('/groups/1/events').then(response => {
+    // 	fetch('/groups/1/events').then(response => {
     //   if(response.ok) {
     //     return (response.json())
     //   }
@@ -25,27 +40,36 @@ class EventsView extends Component {
     //     this.setState({ loading:false })
     //   }
     // });
-    this.setState({ events: [{id:1, name: "Evento del año"}, {id: 2, name: "Evento no tan weno"}] })
   }
-  
-	render() {
-    if(this.state.loading) {
-      return (<div>Loading...</div>)
+
+  render() {
+    const { loadingGroupEvents, groupEvents } = this.state;
+    if (loadingGroupEvents) {
+      return <div>Loading...</div>;
     }
     return (
       <Fragment>
-        <Layout>
-          <h1>Eventos</h1>
-          <ul>
-            {this.state.events.map(event => 
-              <li key={event.id}>{event.name}</li>
-            )}
-          </ul>
-        </Layout>
+        <SidebarPushable>
+          <Sidebar visible={true} as={Menu} vertical inverted>
+            <Menu.Item header event={null}>
+              Eventos
+              <Icon
+                name="plus"
+                onClick={() => console.log("manejar agregar")}
+                link={true}
+              />
+            </Menu.Item>
+            {groupEvents.map(event => (
+              <Menu.Item key={event.id} event={event} onClick={() => {}}>
+                <Icon name="calendar outline" />
+                {event.name}
+              </Menu.Item>
+            ))}
+          </Sidebar>
+        </SidebarPushable>
       </Fragment>
-    )
-    
-	}
+    );
+  }
 }
 
 export default EventsView;
