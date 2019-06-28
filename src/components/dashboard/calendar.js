9eca4957e8bf20e11442d5afe34b0f0ad7260e82
paulinaaraya <pbaraya@uc.cl>
2019-06-28 04:30:39 -0400

    feat: onSelectedEvent - show details

--- src/components/dashboard/calendar.js
+++ src/components/dashboard/calendar.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 
 import moment from 'moment';
 import 'moment/locale/es';
+import { Modal, List, Icon } from 'semantic-ui-react';
+
+/*Todos los moment().add(1, "day") es porque no estamos guardando hora, entonces las fechas se guardan con hora
+00:00:00 y eso moment lo interpreta como el fin del día anterior :c */ 
 
 class DashboardCalendar extends Component {
   constructor(props) {
@@ -12,9 +16,13 @@ class DashboardCalendar extends Component {
       selectedEvent: {id: null}
     };
     this._onView = this._onView.bind(this);
+    this._unSelect = this._unSelect.bind(this);
   }
   _onView(event) {
-    console.log(event);
+    this.setState({ selectedEvent: event.resource });
+  }
+  _unSelect() {
+    this.setState({ selectedEvent: { id: null } });
   }
   render() {
     const localizer = momentLocalizer(moment)
@@ -22,32 +30,63 @@ class DashboardCalendar extends Component {
       return {
         title: event.name,
         start: moment(event.foundationDate).add(1, "day"), end: moment(event.foundationDate).add(1, "day"),
-        allDay: true, resource: event.description
+        allDay: true, resource: event
       }
     });
-    console.log(events);
+    const selectedEvent = this.state.selectedEvent;
     return (
-      <div style={{height: "38em"}}>
-        <Calendar
-          localizer={localizer}
-          events={events}
-          startAccessor="start"
-          endAccessor="end"
-          views={["month", "week", "agenda"]}
-          onView={this._onView}
-          messages={{
-            today: "Hoy",
-            next: "Próximo",
-            previous: "Anterior",
-            month: "Mes",
-            week: "Semana",
-            allDay: "Todo el día",
-            date: "Fecha",
-            time: "Hora",
-            event: "Evento"
-          }}
-        />
-      </div>
+      <Fragment>
+        {!!this.state.selectedEvent.id &&
+        <Modal
+          open={!!this.state.selectedEvent.id}
+          onClose={this._unSelect}
+          closeIcon
+        >
+          <Modal.Header>{selectedEvent.name}</Modal.Header>
+          <Modal.Content>
+            <List>
+              <List.Item>
+                <Icon name="calendar" />
+                Fecha: {moment(selectedEvent.foundationDate).add(1, "day").format('LL')}
+              </List.Item>
+              <List.Item>
+                <Icon name="question circle" />
+                Descripción: {selectedEvent.description}
+              </List.Item>
+              <List.Item>
+                <Icon name="point" />
+                Ubicación: {selectedEvent.location}
+              </List.Item>
+              <List.Item>
+                <Icon name="dollar" />
+                Precio: {selectedEvent.price !== 0 ? selectedEvent.price : `gratis`}
+              </List.Item>
+            </List>
+          </Modal.Content>
+        </Modal>
+        }
+        <div style={{height: "38em"}}>
+          <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            views={["month", "week", "agenda"]}
+            onSelectEvent={this._onView}
+            messages={{
+              today: "Hoy",
+              next: "Próximo",
+              previous: "Anterior",
+              month: "Mes",
+              week: "Semana",
+              allDay: "Todo el día",
+              date: "Fecha",
+              time: "Hora",
+              event: "Evento"
+            }}
+          />
+        </div>
+      </Fragment>
     )
   }
 }
